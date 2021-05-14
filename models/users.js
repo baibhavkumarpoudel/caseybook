dea6c43a859cc75222b89bbb5169fca6f366318b
Baibhav K Poudel <bkpoudel@up.com>
2021-05-14 11:32:41 -0500

    User model in place

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const users = 
+[
+    {
+    username: "CRHarding",
+    email: "c@c.com",
+    password: "casey123",
+    aboutMe: "Lorem ipsum dolor sit amet dolor lorem sit amet",
+    img: "https://www.asme.org/getmedia/6854a06f-1512-4843-995f-e406eeb867b2/7-questions-with-bill-nye-the-science-guy_hero.jpg.aspx?width=460&height=360&ext=.jpg",
+    uuid: 12345
+    }
+]
+
+module.exports = users
